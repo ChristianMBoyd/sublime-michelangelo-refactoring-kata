8fdf592538cb6bd69e27083f55617fea73d1a520
ChristianMBoyd <134218436+ChristianMBoyd@users.noreply.github.com>
2025-09-21 19:02:28 -0500

    Initial skeleton with minor Dockerfile fix (#1)
    * Added (empty) /deps, /tests, download_dependencies.sh, and started changelog
    
    * Added subdirectory arguments to git clone, instead of cd-ing into deps/
    
    * Minor README improvements
    
    * Updated changelog, no tests content yet to track
    
    * Moved Dockerfile to docker/build-sm64-from-deps and added a docker compose make target.

---
+++ Makefile
@@ -0,0 +1,2 @@
+build-from-deps:
+	docker compose -f docker/build-sm64-from-deps/docker-compose.yaml up --build -d
---
+++ download_dependencies.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Pull in the decomp reference code
+mkdir -p deps/sm64
+git clone https://github.com/n64decomp/sm64.git deps/sm64
